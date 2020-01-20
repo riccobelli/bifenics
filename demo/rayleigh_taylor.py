1859da13de644e0766aff725ae3750e3adbbf25d
driccobelli <davide.riccobelli@gmail.com>
2020-01-20 18:25:54 +0100

    Slight modification

--- demo/rayleigh_taylor.py
+++ demo/rayleigh_taylor.py
@@ -1,7 +1,8 @@
 from minics import NonlinearProblem, ParameterContinuation
 from dolfin import RectangleMesh, Point, VectorFunctionSpace, grad, Identity,\
-    derivative, dx, tr, Constant
+    derivative, dx, tr, Constant, ln, det
 import numpy
+
 
 class RayleighTaylor(NonlinearProblem):
 
@@ -9,19 +10,28 @@ class RayleighTaylor(NonlinearProblem):
         self.L = L
         self.H = H
 
+        # Elastic constants
+        self.mu = Constant(1)
+        self.K = Constant(10)
+
     def mesh(self):
-        return RectangleMesh(Point((0, 0)), Point((self.L, self.H)), 5, 5)
+        return RectangleMesh(Point((0, 0)), Point((self.L, self.H)), 10, 10)
 
     def function_space(self, mesh):
         return VectorFunctionSpace(mesh, "CG", 1)
 
-    def parameter(self):
-        return {"gamma" = Constant(0)}
+    def parameters(self):
+        return {"gamma": Constant(0)}
 
     def residual(self, u, v, g):
         F = Identity(2) + grad(u)
         C = F.T*F
-        I1 = tr()
+        J = det(F)
+        W = self.mu*(tr(C) - 2*ln(J)) + self.k*ln(J)*ln(J)
+
+        psi = W*dx
+
+        return derivative(psi, u, v)
 
 
 rt = RayleighTaylor(1, 1)
--- minics/continuation_methods.py
+++ minics/continuation_methods.py
@@ -5,15 +5,24 @@ import matplotlib.pyplot as plt
 
 class ParameterContinuation(object):
 
-    def __init__(self, problem):
+    def __init__(self, problem, param_name, param_start, param_end, dt):
         self.problem = problem
+        self._param_name = param_name
+        self._param_start = param_start
+        self._param_end = param_end
+        self._dt = dt
 
     def run(self):
+        # Setting mesh and defining functional spaces edqwop eqowkel epoqwke eqpowk eqpowke
         mesh = self.problem.mesh()
         V = self.problem.function_space(mesh)
-        params = self.problem.parameters()
         u = Function(V)
         v = TestFunction(V)
+
+        # Setting parameters values
+        self.param = self.problem.parameters()[self.param_name]
+        self.param.assign(self._param_start)
+
         F = self.problem.residual(u, v, params)
 
         plt.show()
