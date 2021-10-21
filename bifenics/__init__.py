a2461cd3710856ca21ccbe496f9566e3613b758c
riccobelli <davide.riccobelli@gmail.com>
2021-10-21 11:58:13 +0200

    __init__.py is consistent with master

--- bifenics/__init__.py
+++ bifenics/__init__.py
@@ -1,5 +1,5 @@
-from bifenics.parameter_continuation import ParameterContinuation
 from bifenics.arclength_continuation import ArclengthContinuation
+from bifenics.parameter_continuation import ParameterContinuation
 from bifenics.nonlinear_problem import BifenicsProblem
 
 from bifenics.utilities import evaluate_function
