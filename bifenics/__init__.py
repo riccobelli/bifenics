659900773c088ef1352aaee52093ca840ac12fe2
riccobelli <davide.riccobelli@gmail.com>
2021-10-21 10:30:42 +0200

    Split the continuation methods into two dinstict files

--- bifenics/__init__.py
+++ bifenics/__init__.py
@@ -1,4 +1,5 @@
-from bifenics.continuation_methods import ParameterContinuation, ArclengthContinuation
+from bifenics.parameter_continuation import ParameterContinuation
+from bifenics.arclength_continuation import ArclengthContinuation
 from bifenics.nonlinear_problem import BifenicsProblem
 
 from bifenics.utilities import evaluate_function
